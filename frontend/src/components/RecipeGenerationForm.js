e61cef67affb3675927bd1d8bea46459eebd2e9f
Isss11 <isaiah.sinclair.0711@gmail.com>
2025-05-03 12:38:31 -0400

    Add Basic Form Input

--- frontend/src/components/RecipeGenerationForm.js
+++ frontend/src/components/RecipeGenerationForm.js
@@ -1,6 +1,14 @@
 import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { useState, useEffect } from "react";
+import "./recipe-generation-form.css";
 
 const RecipeGenerationForm = () => {
+  const [culturesText, setCulturesText] = useState("");
+  const [ingredientsText, setIngredientsText] = useState("");
+  const [cultures, setCultures] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+
   const generateRecipes = async (e) => {
     e.preventDefault();
 
@@ -25,8 +33,34 @@ const RecipeGenerationForm = () => {
     }
   };
 
+  useEffect(() => {
+    try {
+      setCultures(culturesText.split(","));
+    } catch (e) {
+      console.log("Did not update cultures.");
+    }
+  }, [culturesText]);
+
+  useEffect(() => {
+    try {
+      setIngredients(ingredientsText.split(","));
+    } catch (e) {
+      console.log("Did not update ingredients.");
+    }
+  }, [ingredientsText]);
+
   return (
-    <form>
+    <form id="recipe-form">
+      <InputText
+        placeholder="Korean, Chinese, etc."
+        value={culturesText}
+        onChange={(e) => setCulturesText(e.target.value)}
+      />
+      <InputText
+        placeholder="apple, roast chicken, pizza, etc."
+        value={ingredientsText}
+        onChange={(e) => setIngredientsText(e.target.value)}
+      />
       <Button
         label="Generate Recipes"
         severity="Primary"
