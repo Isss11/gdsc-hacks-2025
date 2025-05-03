ed3544881b641c5ae8536f0f7cd6ccb5629c2da0
Isss11 <isaiah.sinclair.0711@gmail.com>
2025-05-03 10:47:22 -0400

    Implement Basic Endpoint

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['POST'])
+def recipe(request):
+    print(request.method)
+    
+    if request.method == "POST":
+        return JsonResponse({"name": "Dhruvesh's Daal Makhni"}, status=200)
