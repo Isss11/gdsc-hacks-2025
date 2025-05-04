233eb115300114fc60798aa3ea9252ef35e382ce
Isss11 <isaiah.sinclair.0711@gmail.com>
2025-05-03 20:57:31 -0400

    Add Logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,16 @@
+import applicationLogo from "../assets/apple-bite.png";
+
 const Header = () => {
   return (
     <header>
-      <h1 id="title">LeftoverMesh</h1>
+      <div id="title-banner">
+        <img
+          src={applicationLogo}
+          alt="LeftoverMesh Logo"
+          id="title-logo"
+        ></img>
+        <h1>LeftoverMesh</h1>
+      </div>
     </header>
   );
 };
